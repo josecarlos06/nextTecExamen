ca7282cc005c9d2ecf7dacf29bd78d51da3b82de
josecarlos06 <carlosmedina.vasq06@gmail.com>
2025-03-28 13:21:57 -0600

    Agregar componente Navbar con estilos y funcionalidad, y actualizar la página de inicio con contenido y estructura

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { abecedarioUIColores } from '../../utils/index';
 
 @Component({
   selector: 'app-navbar',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  title = "Carlos Medina";
 }
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface Job {
+  puesto: string,
+  empresa: string,
+  id: string
+}
+
 @Component({
   selector: 'app-home-page',
   standalone: true,
@@ -8,5 +14,34 @@ import { Component } from '@angular/core';
   styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
+  job = <Job[]>[];
 
+  constructor() {
+    this.getUserNames()
+  }
+
+  async getUserNames() {
+    try {
+      const res = await fetch('https://67d0ea93825945773eb24739.mockapi.io/api/v1/getAllJobs#');
+      const data = await res.json();
+      this.job = data
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  editUserName(job : Job){
+    const index = this.job.findIndex((u) => u.id === job.id);
+    this.job[index] = {...this.job[index] , ...job } ;
+  };
+
+  deleteUser(id : string){
+    const filters = this.job.filter((u) => u.id !== id);
+    this.job = filters;
+  };
+
+  addUsers(job : Job){
+    const data = [...this.job, job]
+    this.job = data;
+  };
 }
